50832c8c8faa5d6c92f573ce9d077a142ee57041
PlaksinaKate <plaksina.larisa@yandex.ru>
2020-10-27 21:25:32 +0300

    Initial

--- src/com/company/lineDrawers/WuLineDrawer.java
+++ src/com/company/lineDrawers/WuLineDrawer.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public class WuLineDrawer implements LineDrawer {
     private PixelDrawer pd;
+    private int xStart, xFinish, yStart, yFinish;
 
     public WuLineDrawer(PixelDrawer pd) {
         this.pd = pd;
@@ -25,11 +26,11 @@ public class WuLineDrawer implements LineDrawer {
             dy = y1 - y2;
             dx = x1 - x2;
         }
-
+        int count = 1;
         int d0 = 0;
         if (dx > dy) {
             for (int i = 0; i <= dx; i++) {
-                drawPixelAsWu(x1, y1, x2, y2, dx, dy, d0);
+                drawPixelAsWu(x1, y1, x2, y2, dx, dy, d0, count);
                 d0 += 2 * dy;
                 if (y1 > y2 && d0 > dx) {
                     y1--;
@@ -43,10 +44,11 @@ public class WuLineDrawer implements LineDrawer {
                 } else {
                     x1++;
                 }
+                count++;
             }
         } else {
             for (int i = 0; i <= dy; i++) {
-                drawPixelAsWu(x1, y1, x2, y2, dx, dy, d0);
+                drawPixelAsWu(x1, y1, x2, y2, dx, dy, d0, count);
                 d0 += 2 * dx;
                 if (x1 > x2 && d0 > dy) {
                     x1--;
@@ -60,58 +62,68 @@ public class WuLineDrawer implements LineDrawer {
                 } else {
                     y1++;
                 }
+                count++;
             }
         }
     }
 
-    public void drawPixelAsWu(int x1, int y1, int x2, int y2, int dx, int dy, int d0) {
+    public void drawPixelAsWu(int x1, int y1, int x2, int y2, int dx, int dy, int d0, int count) {
         int d;
-        boolean replace = Math.abs(dx) > Math.abs(dy);
+        boolean replace = dx > dy;
         if (replace) {
             d = dx != 0 ? (255 * d0) / (2 * dx) : 255;
         } else {
             d = dy != 0 ? (255 * d0) / (2 * dy) : 255;
         }
         int dPos = Math.max(0, d);
-//        int dNeg = Math.max(0, -d);
-//        int dMax = 255 - Math.abs(d);
 
         Color c1 = setColor(255 - Math.abs(d), Color.BLUE);
         Color c2 = setColor(Math.abs(d), Color.BLUE);
 
-
+        if (count == 1) {
+            xStart = x1;
+            xFinish = x2;
+            yStart = y1;
+            yFinish = y2;
+        }
         if (replace) {
             pd.drawPixel(x1, y1, c1);
             if (dx != 0) {
-                if (y1 > y2) {
+                if (y1 > y2 || (yStart > yFinish && y1 == y2)) {
+
                     if (dPos > 0)
                         pd.drawPixel(x1, y1 - 1, c2);
                     else
                         pd.drawPixel(x1, y1 + 1, c2);
-                } else {
+
+                } else if (y1 < y2 || (yStart < yFinish && y1 == y2)) {
+
                     if (dPos > 0)
                         pd.drawPixel(x1, y1 + 1, c2);
                     else
                         pd.drawPixel(x1, y1 - 1, c2);
+
                 }
             }
         } else {
             pd.drawPixel(x1, y1, c1);
             if (dy != 0) {
-                if (x1 >= x2) {
-                    if (dPos > 0)
-                        pd.drawPixel(x1 - 1, y1, c2);
-                    else
+                if (x1 < x2 || (xStart < xFinish && x1 == x2)) {
+                    if (dPos > 0) {
                         pd.drawPixel(x1 + 1, y1, c2);
-                } else {
-                    if (dPos > 0)
-                        pd.drawPixel(x1 + 1, y1, c2);
-                    else
+                    } else {
                         pd.drawPixel(x1 - 1, y1, c2);
+                    }
+                } else if (x1 > x2 || (xStart > xFinish && x1 == x2)) {
+                    if (dPos > 0) {
+                        pd.drawPixel(x1 - 1, y1, c2);
+                    } else {
+                        pd.drawPixel(x1 + 1, y1, c2);
+                    }
                 }
-
             }
         }
+
     }
 
     private Color setColor(int n, Color c) {
